682136b1e5b66c96adce035989bd99149e53df0b
gabidavila <gabidavila@gmail.com>
2017-07-25 23:48:31 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    id = row.pop
-    row << id
-    self.new(*row)
+    self.new(row[1], row[2], row[0])
   end
 
   def self.find_by_name(name)
